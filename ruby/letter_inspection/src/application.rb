ac0a3c3b04bd2ee39106a455a81a41b805434d4e
Hayato Ishida <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-19 23:48:19 +0900

    Update interface of method

--- ruby/letter_inspection/src/application.rb
+++ ruby/letter_inspection/src/application.rb
@@ -1,17 +1,21 @@
 module LetterInspection
   class Application
-    def initialize(str_1, str_2)
-      @str_1 = str_1
-      @str_2 = str_2
+    def self.exactly_equal_size_and_included?(source:, target:)
+      new(source, target).exactly_equal_size_and_included?
+    end
+
+    def initialize(source, target)
+      @source = source
+      @target = target
     end
 
     def exactly_equal_size_and_included?
-      sort_string(str_1) == sort_string(str_2)
+      sort_string(source) == sort_string(target)
     end
 
     private
 
-    attr_reader :str_1, :str_2
+    attr_reader :source, :target
 
     def sort_string(str)
       str.split('').sort.join('')
