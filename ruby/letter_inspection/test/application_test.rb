8d8dd609d0427c5137c1e0dfdf74e308dd629e59
Hayato Ishida <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-19 23:48:57 +0900

    Abort subclasses to simplify unit tests

--- ruby/letter_inspection/test/application_test.rb
+++ ruby/letter_inspection/test/application_test.rb
@@ -2,37 +2,15 @@ require 'minitest/autorun'
 require_relative '../src/application'
 
 class LetterInspection::ApplicationTest < Minitest::Test
-  def setup
-    str_1      = 'hogefoobar'
-    str_2      = 'abefghooor'
-    str_3      = 'hoge'
-    str_4      = 'piyopoopee'
-    @pattern_1 = ::LetterInspection::Application.new(str_1, str_2)
-    @pattern_2 = ::LetterInspection::Application.new(str_1, str_3)
-    @pattern_3 = ::LetterInspection::Application.new(str_1, str_4)
+  def test_exactly_equal_size_and_included
+    assert_equal true,  ::LetterInspection::Application.exactly_equal_size_and_included?(source: 'hogefoobar', target: 'abefghooor')
   end
 
-  private
-
-  attr_reader :pattern_1, :pattern_2, :pattern_3
-end
-
-class RegularCaseTest < LetterInspection::ApplicationTest
-  def test_exactly_equal_size_and_included_1
-    assert_equal true,  pattern_1.exactly_equal_size_and_included?
+  def test_partly_equal_size_and_included
+    assert_equal false,  ::LetterInspection::Application.exactly_equal_size_and_included?(source: 'hogefoobar', target: 'hoge')
   end
-end
 
-class IrregularCaseTest < LetterInspection::ApplicationTest; end
-
-class TestCase1 < IrregularCaseTest
-  def test_exactly_equal_size_and_included_2
-    assert_equal false, pattern_2.exactly_equal_size_and_included?
-  end
-end
-
-class TestCase2 < IrregularCaseTest
-  def test_exactly_equal_size_and_included_3
-    assert_equal false, pattern_3.exactly_equal_size_and_included?
+  def test_not_equal_size_and_included
+    assert_equal false,  ::LetterInspection::Application.exactly_equal_size_and_included?(source: 'hogefoobar', target: 'piyopoopee')
   end
 end
--- ruby/nabeatsu/test/nabeatsu_test.rb
+++ ruby/nabeatsu/test/nabeatsu_test.rb
@@ -1,10 +1,8 @@
 require 'minitest/autorun'
 require_relative '../src/nabeatsu'
 
-class NabeatsuTest < Minitest::Test; end
-
-class IfStatementTest < NabeatsuTest
-  def test_go_crazy
+class NabeatsuTest < Minitest::Test
+  def test_go_crazy_in_if_statement
     1.upto(40).each do |num|
       if num % 3 == 0 || num.to_s.include?('3')
         assert_equal "#{num.to_s}!", go_crazy_in_if(num)
@@ -13,10 +11,8 @@ class IfStatementTest < NabeatsuTest
       end
     end
   end
-end
 
-class TernaryStatementTest < NabeatsuTest
-  def test_go_crazy
+  def test_go_crazy_in_ternary_statement
     1.upto(40).each do |num|
       if num % 3 == 0 || num.to_s.include?('3')
         assert_equal "#{num.to_s}!", go_crazy_in_ternary(num)
